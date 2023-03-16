eca222a8487ddefdda973dc33cc93e9e4a98bc86
root <you@example.com>
2023-03-16 03:34:06 -0700

    101-nqueens.py

--- python-more_classes/101-nqueens.py
+++ python-more_classes/101-nqueens.py
@@ -1,22 +1,8 @@
 #!/usr/bin/python3
-# The N queens puzzle is the challenge of placing N non-attacking queens on an N×N chessboard.
+# The N queens puzzle is the challenge of placing N non-attacking queens on an N×N chessboad.
 """Solves the N-queens puzzle.
-
 Determines all possible solutions to placing N
 N non-attacking queens on an NxN chessboard.
-
-Example:
-    $ ./101-nqueens.py N
-
-N must be an integer greater than or equal to 4.
-
-Attributes:
-    board (list): A list of lists representing the chessboard.
-    solutions (list): A list of lists containing solutions.
-
-Solutions are represented in the format [[r, c], [r, c], [r, c], [r, c]]
-where `r` and `c` represent the row and column, respectively, where a
-queen must be placed on the chessboard.
 """
 import sys
 
