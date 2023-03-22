9f23614d3c2a473bbf92335a90e4896d02b37074
root <you@example.com>
2023-03-22 06:38:07 -0700

    1-write_file.py

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" function that writes a string to a text file (UTF8) and
+returns the number of characters written"""
+
+
+def number_of_lines(filename=""):
+    """Return the number of lines in a text file."""
+    lines = 0
+    with open(filename) as f:
+        for line in f:
+            lines += 1
+    return lines
