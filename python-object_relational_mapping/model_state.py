e7fb13df4777cd13d92f187b6214d16db0e177ca
root <you@example.com>
2023-04-22 08:37:50 -0700

    model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,12 +1,35 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
 """
-Start link class to table in database
+This module initializes a database table named `states` by linking a corresponding SQLAlchemy model class to the database.
+
+Usage: python3 create_states_table.py <username> <password> <database_name>
+
+The `create_engine` function is used to create a connection to the MySQL database. The connection string is formatted using the command-line arguments passed to the script.
+
+The `Base.metadata.create_all` function creates all the tables that have been defined in the SQLAlchemy model. In this case, it creates a table for the `State` model in `model_state.py`.
+
+Example usage:
+
+    $ python3 create_states_table.py root password mydatabase
+
 """
+
 import sys
+from sqlalchemy import create_engine
 from model_state import Base, State
 
-from sqlalchemy import (create_engine)
-
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    # Check if the correct number of command-line arguments were provided
+    if len(sys.argv) != 4:
+        print("Usage: python3 create_states_table.py <username> <password> <database_name>")
+        sys.exit(1)
+
+    # Create a connection to the database using the command-line arguments
+    username, password, database_name = sys.argv[1:]
+    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost/{database_name}', pool_pre_ping=True)
+
+    # Create the table in the database
     Base.metadata.create_all(engine)
+
+    print("Table 'states' has been created in the database.")
