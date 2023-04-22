1348e372f8a7871d44fe81017b352b1017ffc60e
root <you@example.com>
2023-04-22 08:47:54 -0700

    6-model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,35 +1,20 @@
 #!/usr/bin/python3
+# Defines a State model.
+# Inherits from SQLAlchemy Base and links to the MySQL table states.
 
-"""
-This module initializes a database table named `states` by linking a corresponding SQLAlchemy model class to the database.
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-Usage: python3 create_states_table.py <username> <password> <database_name>
+Base = declarative_base()
 
-The `create_engine` function is used to create a connection to the MySQL database. The connection string is formatted using the command-line arguments passed to the script.
 
-The `Base.metadata.create_all` function creates all the tables that have been defined in the SQLAlchemy model. In this case, it creates a table for the `State` model in `model_state.py`.
+class State(Base):
+    """Represents a state for a MySQL database.
 
-Example usage:
-
-    $ python3 create_states_table.py root password mydatabase
-
-"""
-
-import sys
-from sqlalchemy import create_engine
-from model_state import Base, State
-
-if __name__ == "__main__":
-    # Check if the correct number of command-line arguments were provided
-    if len(sys.argv) != 4:
-        print("Usage: python3 create_states_table.py <username> <password> <database_name>")
-        sys.exit(1)
-
-    # Create a connection to the database using the command-line arguments
-    username, password, database_name = sys.argv[1:]
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost/{database_name}', pool_pre_ping=True)
-
-    # Create the table in the database
-    Base.metadata.create_all(engine)
-
-    print("Table 'states' has been created in the database.")
+    __tablename__ (str): The name of the MySQL table to store States.
+    id (sqlalchemy.Integer): The state's id.
+    name (sqlalchemy.String): The state's name.
+    """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
--- python-object_relational_mapping/model_state.sql
+++ python-object_relational_mapping/model_state.sql
@@ -1,4 +1,5 @@
--- Create database hbtn_0e_6_usa
+# This SQL script creates a new database named 'hbtn_0e_6_usa', selects it for use, and then displays the SQL code used to create the 'states' table.
 CREATE DATABASE IF NOT EXISTS hbtn_0e_6_usa;
 USE hbtn_0e_6_usa;
 SHOW CREATE TABLE states;
+
