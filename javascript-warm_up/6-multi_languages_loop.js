6b16948ae624049c61f61a2d03b6ef1402e9e294
root <you@example.com>
2023-06-07 08:20:02 -0700

    6-multi_languages_loop.js

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const lang = ['C is fun', 'Pythron is cool', 'JavaScript is amazing'];
+const lang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let a = 0;
 for (a = 0; a <= 2; a++) {
   console.log(lang[a]);
