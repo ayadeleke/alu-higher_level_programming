861814a78d9658463ff0adcb02285f92a08940c8
root <you@example.com>
2023-06-07 07:58:35 -0700

    5-to_integer.js

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
-const args = process.argv;
-const conV = Number(args[2]);
-if (conV) {
-  console.log('My number:' ${args[2]});
+
+const arg = process.argv;
+
+const argNum = Number(arg[2]);
+
+if (argNum) {
+  console.log(`My number: ${args[2]}`);
 } else {
   console.log('Not a number');
 }
