b3eb6b74196f6c2d7c060fe430e98edc8e8976b4
root <you@example.com>
2023-06-08 03:36:27 -0700

    101-sorted.js

---
+++ javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const { dict } = require('./101-data');
+
+function computeUserIdsByOccurrence(dict) {
+  const userIdsByOccurrence = {};
+
+  for (const [userId, occurrence] of Object.entries(dict)) {
+    if (!(occurrence in userIdsByOccurrence)) {
+      userIdsByOccurrence[occurrence] = [];
+    }
+
+    userIdsByOccurrence[occurrence].push(userId);
+  }
+
+  return userIdsByOccurrence;
+}
+
+const userIdsByOccurrence = computeUserIdsByOccurrence(dict);
+
+console.log(userIdsByOccurrence);
